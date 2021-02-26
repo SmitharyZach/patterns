c546d01400a61787db5904d35d659cfef9dd9d5a
Zach Smith <znathanielsmith@gmail.com>
2021-02-25 21:00:38 -0500

    first commit with file structure

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs",
+    defaultLayout: "index",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  console.log("hit homepage");
+  res.render("homepage");
+});
+
+app.listen(port, () => {
+  console.log("servin runnin WILD");
+});
