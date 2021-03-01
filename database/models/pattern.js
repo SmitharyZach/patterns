8e5e461046c7e214d8f16dd272ccf0c9d2faabac
Zach Smith <znathanielsmith@gmail.com>
2021-03-01 07:45:44 -0500

    finish user login and sign up and add session

--- database/models/pattern.js
+++ database/models/pattern.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       models.pattern.belongsTo(models.user);
-      models.patter.hasMany(models.score);
+      models.pattern.hasMany(models.score);
     }
   }
   pattern.init(
--- database/models/user.js
+++ database/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.user.hasMany(models.patterns);
+      models.user.hasMany(models.pattern);
     }
   }
   user.init(
